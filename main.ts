f6652d04e7b982b1fb0c8dd174e50b551c470bac
Charumathi Hariharan <86341150+charuharan@users.noreply.github.com>
2021-07-17 18:23:34 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+let isBeginner: boolean = true;
+let total: number = 0;
+let name1: string = 'Charu';
+let sentence: string = `My name is ${name1}
+I am a begineer in Typescript`;
+console.log(sentence);
